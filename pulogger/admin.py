046eb2f842171b121442f8426402ae1f3d4ae0d5
Alex Dunn <alexdunnmobile@gmail.com>
2019-05-25 20:35:10 -0700

    Updated models, reset db

--- pulogger/admin.py
+++ pulogger/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Datalogger, SensorModel, Sensor
+
+admin.site.register(Datalogger)
+admin.site.register(SensorModel)
+admin.site.register(Sensor)

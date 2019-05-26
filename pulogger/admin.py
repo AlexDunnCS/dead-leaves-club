e6330fc2eb199c51426a3216eeb8da4846022fb7
Alex Dunn <alexdunnmobile@gmail.com>
2019-05-25 20:35:10 -0700

    Updated models, reset db

--- pulogger/admin.py
+++ pulogger/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Datalogger, SensorModel, Sensor
+
+admin.register(Datalogger)
+admin.register(SensorModel)
+admin.register(Sensor)
--- pulogger/models.py
+++ pulogger/models.py
@@ -4,32 +4,66 @@ from django.db import models
 class Datalogger(models.Model):
     device_name = models.CharField(max_length=64)
     description = models.CharField(max_length=64)
-    temperature_sensors = models.SmallIntegerField()
+    temperature_sensor_count = models.SmallIntegerField()
     humidity_sensor_count = models.SmallIntegerField()
     up_since = models.DateTimeField('uninterrupted since')
     last_transmission = models.DateTimeField('last transmission received')
 
+    def __str__(self):
+        return '{} ({}) - {}temp, {}hum, up since {}, last xmit {}'.format(self.device_name, self.description,
+                                                                          self.temperature_sensor_count,
+                                                                          self.humidity_sensor_count,
+                                                                          self.up_since, self.last_transmission)
 
-class SensorDatum(models.Model):
-    device = models.ForeignKey(Datalogger, on_delete=models.DO_NOTHING)
-    ip = models.GenericIPAddressField()
+
+class SensorModel(models.Model):
+    type = models.CharField(max_length=16)
+    description = models.CharField(max_length=64)
+    has_temperature = models.BooleanField()
+    has_humidity = models.BooleanField()
+
+    def __str__(self):
+        sensors = []
+
+        if self.has_temperature:
+            sensors.append("temperature")
+
+        if self.has_humidity:
+            sensors.append("humidity")
+
+        return '{} ({}) - records {}'.format(self.type, self.description, (), ', '.join(sensors))
+
+
+class Sensor(models.Model):
+    datalogger = models.ForeignKey(Datalogger, on_delete=models.CASCADE)
+    type = models.ForeignKey(SensorModel, on_delete=models.PROTECT)
+    description = models.CharField(max_length=64)
+
+    def __str__(self):
+        return '{} ({}) - attached to {}'.format(self.type, self.description, self.datalogger)
+
+
+class AbstractSensorDatum(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
+    submission_ip = models.GenericIPAddressField()
     timestamp = models.DateTimeField()
+
+    def get_abstract_string(self):
+        return '({}, {}, submitted at {} from {} )'.format(self.sensor.datalogger.device_name, self.sensor.description, self.timestamp, self.submission_ip)
 
     class Meta:
         abstract = True
 
 
-class TemperatureDatum(SensorDatum):
+class TemperatureDatum(AbstractSensorDatum):
     temperature = models.DecimalField(max_digits=4, decimal_places=2)
 
+    def __str__(self):
+        return '{} {}'.format(self.temperature, self.get_abstract_string())
 
-class HumidityDatum(SensorDatum):
+
+class HumidityDatum(AbstractSensorDatum):
     humidity = models.DecimalField(max_digits=4, decimal_places=2)
 
-
-# class SensorAlarmEvent(models.Model):
-#     device = models.ForeignKey(Datalogger, on_delete=models.DO_NOTHING)
-#     timestamp = models.DateTimeField()
-#
-#     class Meta:
-#         abstract = True
+    def __str__(self):
+        return '{} {}'.format(self.humidity, self.get_abstract_string())

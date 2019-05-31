799aaecb5a5d51fed8bc2e3d663c06c660680769
Alex Dunn <alexdunnmobile@gmail.com>
2019-05-30 21:30:36 -0700

    Added local ip to allowed-hosts for local debug integration with esp8266

--- dead_leaves_club/settings.py
+++ dead_leaves_club/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')b5cbwmfg$-xz1qj*a@s!w9c6df)t9pqt4hln**u5jd-c(wbkq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.11']
 
 
 # Application definition

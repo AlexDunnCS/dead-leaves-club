f1b2965f245f7ef0f7382026c73f36e1015ba312
Alex Dunn <alexdunnmobile@gmail.com>
2019-07-28 10:22:27 -0700

    Debug is now turned off when running under linux (ie in production env)

--- dead_leaves_club/settings.py
+++ dead_leaves_club/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import secret_config
+from sys import platform
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = secret_config.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False if platform == 'linux' else True
 
 ALLOWED_HOSTS = secret_config.ALLOWED_HOSTS
 
